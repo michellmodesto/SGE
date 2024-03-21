8d22be9ad3b936182c4085c46615f45fb92f9a4a
Michel Modesto <125322105+michellmodesto@users.noreply.github.com>
2024-03-21 20:27:55 -0300

    projetosge

---
+++ SGE/frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { signOut } = useAuth();
+
+
+definePageMeta({
+    middleware: 'auth'
+});
+const logout = ()=>{
+            
+         signOut( {redirect: false})
+        .then(()=>{
+                console.log("successfully logout!!");
+                navigateTo('/');
+            })
+            .catch((error)=>{
+                console.log("Error when trying to execute logout:", error);
+            })
+        }  
+</script>
+
+<template>
+    <div>
+        <h1>Minha Tela Home!!!</h1>
+        <button @click="logout" class=""customButton>SAIR</button>    
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
